6fba3e5c58a4324dbee81275155d87a0ca294de6
Alejandro Loja <alejandro.loja@gmail.com>
2019-09-28 00:41:50 -0400

    Deployment

---
+++ build/precache-manifest.fa87f1f606d1972b6db09c1bc2a70763.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bc34eed4e8f4202d6c2738a4c7f5aa7c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3436f259f23987c4fd1c",
+    "url": "/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "8b8b55180635e2573e62",
+    "url": "/static/js/2.05649392.chunk.js"
+  },
+  {
+    "revision": "3436f259f23987c4fd1c",
+    "url": "/static/js/main.ba8b2a11.chunk.js"
+  },
+  {
+    "revision": "967de4339823c238e2a5",
+    "url": "/static/js/runtime-main.093a27d0.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/static/media/logo.25bf045c.svg"
+  }
+]);
